b88d3449d08d35dc2161538db0502a39f6b236dd
Heder Barreto <matheusmr640@gmail.com>
2023-11-27 16:03:18 -0300

    chamada sql

--- Database/DatabaseNovo.sql
+++ Database/DatabaseNovo.sql
@@ -33,3 +33,17 @@ CREATE TABLE desperdicio_producao_produto (
 );
 
 
+SELECT
+    p.descricao AS nome_produto,
+    g.descricao AS grupo_produto,
+    COUNT(dp.sequencia) AS quantidade_de_lancamentos,
+    SUM(dp.qtdeSaida) AS quantidade_total
+FROM
+    desperdicio_producao_produto dp
+INNER JOIN produtos p ON dp.codTbProduto = p.codigo_produto
+INNER JOIN grupo_produtos g ON p.codTbGrupo = g.codigo_grupo
+GROUP BY
+    p.descricao, g.descricao
+HAVING
+    COUNT(dp.sequencia) > 5;
+
